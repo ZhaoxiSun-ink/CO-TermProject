5001592e1f2ee53f75da2e1a93db3f4fe90dcd66
Vampzmage <57106304+vampzmage@users.noreply.github.com>
2019-12-08 06:17:59 -0500

    file reading function added

--- p1.py
+++ p1.py
@@ -1,2 +1,26 @@
 #Fall 2019 CO term-project
 #Zhaoxi Sun & Hongbao Zhao & Qiran Sun & Yang Yi
+
+import sys
+
+
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+
+def getInstructions(arguments):#Hongbo Zhao
+    branches = {}
+    mode = arguments[0]
+    file = arguments[1]
+    instruction = ""
+    line_num = 0
+    f= open(file)
+    for line in f.readlines():
+        line = line.strip()
+        instruction += line
+        line_num += 1
+        lenOfInst = len(line)
+        if line[lenOfInst-1] == ":":
+            branch_name = line[0,lenOfInst-1]
+            branches[branch_name] = line_num
+    f.close()
+
